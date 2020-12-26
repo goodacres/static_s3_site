e8e4212f4916e3556eb8db0895b2e79a79b7ff69
root <root@centos8-local.home>
2020-12-26 23:22:59 +0000

    v1: Hack from https://github.com/hashicorp/terraform-provider-aws/issues/10098#issuecomment-663562342

--- main.tf
+++ main.tf
@@ -26,10 +26,14 @@ resource "aws_acm_certificate" "wildcard_website" {
 
 # Validates the ACM wildcard by creating a Route53 record (as `validation_method` is set to `DNS` in the aws_acm_certificate resource)
 resource "aws_route53_record" "wildcard_validation" {
-  name    = aws_acm_certificate.wildcard_website.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.wildcard_website.domain_validation_options[0].resource_record_type
-  zone_id = data.aws_route53_zone.main.zone_id
-  records = [aws_acm_certificate.wildcard_website.domain_validation_options[0].resource_record_value]
+  for_each = {
+    for dvo in aws_acm_certificate.example_com.domain_validation_options: dvo.domain_name => {
+      name    = dvo.resource_record_name
+      record  = dvo.resource_record_value
+      type    = dvo.resource_record_type
+      zone_id = data.aws_route53_zone.main.zone_id
+    }
+  }
   ttl     = "60"
 }
 
